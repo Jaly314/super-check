feeafadab9da6bd0f827a9dd01519f95afd33089
Jaly314 <35157483@qq.com>
2020-05-01 19:02:46 +0800

    修改一处标点

--- utils/demo.c
+++ utils/demo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "check.h”
+#include "check.h"			/* super check 头文件 */
 
 
 #define DO_NOTHING ;            /* 无操作 */
@@ -20,7 +20,7 @@ int test_func(int a, int b, int *arr)
 	CHECK(1 + 1, 2, goto exit); 
 
 	*arr = add_func(a, b);
-	CHECK(1 + 1, 3, printf("are you kidding me ?\n"); DO_NOTHING);
+	CHECK(1 + 1, 3, printf("are you kidding me ?\n"), DO_NOTHING);
 
 	return 0;
 
@@ -32,8 +32,8 @@ int main(void)
 {
 	int sum = 0;
 	
-	test_func(6, 8, &sum);
-
+	int ret = test_func(6, 8, &sum);
+	CHECK(ret, 0, printf("test func error\n"));
 	CHECK(sum, 6 + 8, return -1);
 
 	return 0;
